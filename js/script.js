b73a8e77ab2f3c246aa4a307449bdd27fec3ae47
VitorVitima <yukkiegg@gmail.com>
2023-02-24 23:32:08 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,3 @@
 const canvas = document.querySelector('#personagem1')
 const ctx = canvas.getContext('2d')
-let px = 0
-const incrementoAndEncremento = 10
-let volta = false
-function animacao(){
-    ctx.clearRect(0, 0, 400, 400)
-    ctx.fillStyle='#fff'
-    ctx.fillRect(px, 0, 50, 50)
-    if(volta == false){
-        px+= incrementoAndEncremento
-        if(px >= 350){
-            volta = true
-        }
-    } else{
-        px-= incrementoAndEncremento
-        if(px == 0){
-            volta = false
-        }
-    }
-    requestAnimationFrame(animacao)
-}
-animacao()
+
